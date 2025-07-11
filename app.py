3f544398aa27af394cb17ad7348e9a942db6eac0
ARUNAGIRINATHAN_K <167340745+arun-techverse@users.noreply.github.com>
2025-07-11 05:04:10 +0000

    packages

---
+++ app.py
@@ -0,0 +1,5 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+from utils.cartoonify import cartoonify_image
+from werkzeug.utils import secure_filename
+from PIL import Image
